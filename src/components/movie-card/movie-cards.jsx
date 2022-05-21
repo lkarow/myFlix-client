ebc32af9656dc66c23a3eb2f479ff84cc590b894
Lennart <lkarow@online.de>
2022-05-21 17:57:33 +0200

    implement propTypes

--- src/components/movie-card/movie-cards.jsx
+++ src/components/movie-card/movie-cards.jsx
@@ -1,8 +1,27 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 export class MovieCard extends React.Component {
   render() {
     const { movie, onMovieClick } = this.props;
+
     return <div className="movie-card" onClick={() => { onMovieClick(movie); }}>{movie.Title}</div>
   }
 }
+
+MovieCard.propTypes = {
+  movie: PropTypes.shape({
+    Title: PropTypes.string.isRequired,
+    Description: PropTypes.string.isRequired,
+    ImagePath: PropTypes.string.isRequired,
+    Director: PropTypes.shape({
+      Name: PropTypes.string.isRequired,
+      Bio: PropTypes.string.isRequired
+    }),
+    Genre: PropTypes.shape({
+      Name: PropTypes.string.isRequired,
+      Description: PropTypes.string.isRequired
+    })
+  }).isRequired,
+  onMovieClick: PropTypes.func.isRequired
+};
