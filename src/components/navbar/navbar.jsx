626730dd700abd523f8cf16abf8c386120d93401
Lennart <lkarow@online.de>
2022-05-31 18:19:13 +0200

    refactor

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,5 +1,5 @@
 import React, { Fragment } from 'react';
-import { Navbar, Container, Nav } from 'react-bootstrap';
+import { Navbar, Nav } from 'react-bootstrap';
 
 export function Navbar({user}) {
 
@@ -19,26 +19,24 @@ export function Navbar({user}) {
 
   return (
     <Navbar bg="light" expand="lg" className="mb-5">
-      <Container>
-        <Navbar.Toggle aria-controls="navbar-nav" />
-        <Navbar.Collapse>
-            <Nav className="me-auto">
-            {isAuth() &&
-            <Fragment>
-                <Nav.Link href="/">Home</Nav.Link>
-                <Nav.Link href={`/users/${user}`}>Profil</Nav.Link>
-                <Nav.Link onClick={onLoggedOut}>Sign-out</Nav.Link>
-              </Fragment>
-            }
-            {!isAuth() &&
-            <Fragment>
-              <Nav.Link href={'/login'}>Sign-in</Nav.Link>
-              <Nav.Link href={'/register'}>Sign-up</Nav.Link>
+      <Navbar.Toggle aria-controls="navbar-nav" />
+      <Navbar.Collapse>
+          <Nav className="me-auto">
+          {isAuth() &&
+          <Fragment>
+              <Nav.Link className="color-nav" href="/">Home</Nav.Link>
+              <Nav.Link href={`/users/${user}`}>Profil</Nav.Link>
+              <Nav.Link onClick={onLoggedOut}>Sign-out</Nav.Link>
             </Fragment>
-            }
-            </Nav>
-        </Navbar.Collapse>
-      </Container>
+          }
+          {!isAuth() &&
+          <Fragment>
+            <Nav.Link href={'/login'}>Sign-in</Nav.Link>
+            <Nav.Link href={'/register'}>Sign-up</Nav.Link>
+          </Fragment>
+          }
+          </Nav>
+      </Navbar.Collapse>
     </Navbar>
   )
 }
