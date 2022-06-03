39756757ab94569e9ebb247bf015aab93ede2061
Lennart <lkarow@online.de>
2022-06-03 22:28:50 +0200

    refactor to async await

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -7,17 +7,17 @@ import { Button, Col, Container, Form, Row } from 'react-bootstrap/';
 import './login-view.scss';
 
 export function LoginView(props) {
-  const [ username, setUsername ] = useState('');
-  const [ password, setPassword ] = useState('');
-  const [ usernameErr, setUsernameErr ] = useState('');
-  const [ passwordErr, setPasswordErr ] = useState('');
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [usernameErr, setUsernameErr] = useState('');
+  const [passwordErr, setPasswordErr] = useState('');
 
   // validate user inputs
   const validate = () => {
     let isReq = true;
     if (!username) {
       setUsernameErr('Username required');
-      isReq= false;
+      isReq = false;
     } else if (username.length < 2) {
       setUsernameErr('Username must be at least 2 characters long');
       isReq = false;
@@ -30,24 +30,26 @@ export function LoginView(props) {
       isReq = false;
     }
     return isReq;
-  }
+  };
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
     const isReq = validate();
     if (isReq) {
-      // Send a request to the server for authentication
-      axios.post('https://movie-api-93167.herokuapp.com/login', {
-        Username: username,
-        Password: password
-      })
-      .then(response => {
+      try {
+        // Send a request to the server for authentication
+        let response = await axios.post(
+          'https://movie-api-93167.herokuapp.com/login',
+          {
+            Username: username,
+            Password: password,
+          }
+        );
         const data = response.data;
         props.onLoggedIn(data);
-      })
-      .catch(e => {
-        console.log('No such user')
-      });
+      } catch (e) {
+        console.log('No such user');
+      }
     }
   };
 
@@ -58,19 +60,29 @@ export function LoginView(props) {
           <Form>
             <Form.Group controlId="formUsername">
               <Form.Label>Username:</Form.Label>
-              <Form.Control type="text" onChange={e => setUsername(e.target.value)} placeholder="Username" />
+              <Form.Control
+                type="text"
+                onChange={(e) => setUsername(e.target.value)}
+                placeholder="Username"
+              />
               {/* display validation error */}
               {usernameErr && <p>{usernameErr}</p>}
             </Form.Group>
             <Form.Group controlId="formPassword" className="mt-3">
               <Form.Label>Password:</Form.Label>
-              <Form.Control type="password" onChange={e => setPassword(e.target.value)} placeholder="Password" />
+              <Form.Control
+                type="password"
+                onChange={(e) => setPassword(e.target.value)}
+                placeholder="Password"
+              />
               {/* display validation error */}
               {passwordErr && <p>{passwordErr}</p>}
             </Form.Group>
             <Row className="mt-3 justify-content-start">
               <Col sm="10" md="8" lg="6">
-                <Button  variant="warning" type="submit" onClick={handleSubmit}>Login</Button>
+                <Button variant="warning" type="submit" onClick={handleSubmit}>
+                  Login
+                </Button>
               </Col>
             </Row>
           </Form>
@@ -85,5 +97,5 @@ LoginView.propTypes = {
     Username: PropTypes.string.isRequired,
     Password: PropTypes.string.isRequired,
   }),
-  onLoggedIn: PropTypes.func.isRequired
-}
+  onLoggedIn: PropTypes.func.isRequired,
+};
